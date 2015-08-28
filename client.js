461d1e26cbb5518b8c642e784b684864bfb600ae
Daniel Hoffmann <daniel.hoffmann@jtl-software.com>
2015-08-28 09:20:09 +0200

    initial commit

---
+++ client.js
@@ -0,0 +1,116 @@
+var entries = [];
+var editor;
+var ajax;
+
+function getContent(timestamp, pointer) {
+    var queryString = {
+        'timestamp' : timestamp,
+        'pointer': pointer,
+        'action': 'run'
+    };
+
+    ajax = $.ajax({
+        type: 'GET',
+        url: 'server.php',
+        data: queryString,
+        success: function(data){
+            $.each(data.data, function(index, el) {
+                var color = '';
+                var icon = '';
+
+                switch(el.label) {
+                    case 'Result':
+                        color = 'label-success';
+                        icon = 'glyphicon-open';
+                        break;
+                    case 'Error':
+                        color = 'label-danger';
+                        icon = 'glyphicon-alert';
+                        break;
+                    default:
+                        color = 'label-primary';
+
+                        if (el.label.indexOf('pull') > -1) {
+                            icon = 'glyphicon-arrow-left';
+                        } else if (el.label.indexOf('push') > -1) {
+                            icon = 'glyphicon-arrow-right';
+                        } else if (el.label.indexOf('delete') > -1) {
+                            icon = 'glyphicon-trash';
+                        } else if (el.label.indexOf('statistic') > -1) {
+                            icon = 'glyphicon-stats';
+                        } else if (el.label.indexOf('auth') > -1) {
+                            icon = 'glyphicon-lock';
+                        } else if (el.label.indexOf('identify') > -1) {
+                            icon = 'glyphicon-info-sign';
+                        } else if (el.label.indexOf('clear') > -1) {
+                            icon = 'glyphicon-refresh';
+                        }
+                        break;
+                }
+
+                var iconStr = '<span class="label '+color+'"><i class="glyphicon '+icon+'"></i></span>&nbsp;&nbsp;';
+                var entry = $('<li><a href="#" id="'+(entries.length)+'">'+iconStr+el.label+'<span class="label label-default pull-right">'+el.timestamp+'</span></a></li>');
+
+                entries.push(el);
+
+                $('#entries').prepend(entry);
+
+                entry.animate({
+                    'background-color': '#272822'
+                }, 5000);
+            });
+
+            if (entries.length > 0) {
+                $('#entries li.active').css('background-color','#272822').removeClass('active');
+
+                editor.setValue(JSON.stringify(entries[entries.length-1].data, null, '\t'), -1);
+                $('#'+(entries.length-1)).parent().addClass('active').stop().animate({
+                    'background-color': '#111111'
+                }, 6000);
+            }
+
+            getContent(data.timestamp, data.pointer);
+        }
+    });
+}
+
+$(function() {
+    editor = ace.edit("view");
+    editor.getSession().setMode("ace/mode/json");
+    editor.setTheme("ace/theme/monokai");
+    editor.setReadOnly(true);
+    editor.setShowPrintMargin(false);
+
+    $('#entries').on('click', 'a', function(e) {
+        $('#entries li.active').removeClass('active');
+        $('#entries li').stop().css('background-color','#272822');
+        editor.setValue(JSON.stringify(entries[$(this).attr('id')].data, null, '\t'), -1);
+        $(this).parent().stop().animate({
+           'background-color': '#111111'
+        }, 500).addClass('active');
+    });
+
+    $('#startBtn').click(function() {
+        if(ajax) {
+            ajax.abort();
+        }
+
+        $('#entries').empty();
+        entries = [];
+
+        getContent(0, 0);
+    });
+
+    $('#resetBtn').click(function() {
+        editor.setValue('');
+
+        ajax = $.ajax({
+            type: 'GET',
+            url: 'server.php',
+            data: {'action': 'reset'},
+            success: function(data) {
+                $('#startBtn').click();
+            }
+        });
+    });
+});
---
+++ server.php
@@ -0,0 +1,90 @@
+<?php
+class Viewer
+{
+    public static function run()
+    {
+        //set_time_limit(0);
+
+        $handle = @fopen("../rpc.log", "r");
+
+        if (!isset($_GET['pointer']) || $_GET['pointer'] == 0) {
+            $_GET['pointer'] = 0;
+        }
+
+        while (true) {
+            $last_ajax_call = isset($_GET['timestamp']) ? (int)$_GET['timestamp'] : null;
+
+            clearstatcache();
+
+            $last_change_in_data_file = filemtime("../rpc.log");
+
+            if ($last_ajax_call == null || $last_change_in_data_file > $last_ajax_call) {
+                fseek($handle, $_GET['pointer']);
+
+                $data = array();
+
+                while (($buffer = fgets($handle, 4096)) !== false) {
+                    $timestampRegEx = '/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/';
+                    $requestRegEx = '/rpc\.DEBUG: RequestPacket: {"method":"(.*)","params":(.*),"jtlrpc"/';
+                    $resultRegEx = '/rpc.DEBUG: {"result":(.*),"error":(.*),"jtlrpc"/';
+
+                    preg_match($timestampRegEx, $buffer, $timestamp);
+
+                    if (preg_match($requestRegEx, $buffer, $request) !== 0) {
+                        $data[] = array(
+                            'type' => 'request',
+                            'label' => $request[1],
+                            'timestamp' => $timestamp[4] . ':' . $timestamp[5] . ':' . $timestamp[6],
+                            'data' => json_decode(stripslashes(trim($request[2], '"')))
+                        );
+                    } elseif (preg_match($resultRegEx, $buffer, $result) !== 0) {
+                        if ($result[2] !== 'null') {
+                            $data[] = array(
+                                'type' => 'error',
+                                'label' => 'Error',
+                                'timestamp' => $timestamp[4] . ':' . $timestamp[5] . ':' . $timestamp[6],
+                                'data' => json_decode($result[2])
+                            );
+                        } else {
+                            $data[] = array(
+                                'type' => 'result',
+                                'label' => 'Result',
+                                'timestamp' => $timestamp[4] . ':' . $timestamp[5] . ':' . $timestamp[6],
+                                'data' => json_decode(stripslashes($result[1]))
+                            );
+                        }
+                    }
+                }
+
+                $result = array(
+                    'data' => $data,
+                    'timestamp' => $last_change_in_data_file,
+                    'lastPointer' => $_GET['pointer'],
+                    'pointer' => ftell($handle)
+                );
+
+                header('Content-Type: application/json');
+                echo json_encode($result);
+
+                break;
+            } else {
+                sleep(1);
+                continue;
+            }
+        }
+    }
+
+    public static function reset()
+    {
+        unlink("../rpc.log");
+    }
+}
+
+switch($_GET['action']) {
+    case 'run':
+        Viewer::run();
+        break;
+    case 'reset':
+        Viewer::reset();
+        break;
+}
