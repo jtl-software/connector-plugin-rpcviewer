f855f84756cbabbcabe56ed6770908862d171dcd
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2019-12-12 15:42:33 +0100

    Show complete response in case an error occurred

--- Listener/RpcListener.php
+++ Listener/RpcListener.php
@@ -39,14 +39,18 @@ class RpcListener
 
     public function afterAction(RpcEvent $event)
     {
-        $data = $event->getData()['result'] ?? [];
+        $data = $event->getData();
+        $showData = $event->getData()['result'] ?? [];
+        if(isset($data['error']) && !is_null($data['error'])) {
+            $showData = $data;
+        }
 
         $entry = [
             'type' => 'result',
             'controller' => $event->getController(),
             'action' => $event->getAction(),
             'timestamp' => date('H:i:s', time()),
-            'data' => $data
+            'data' => $showData
         ];
 
         fwrite($this->json, json_encode($entry)."\n");
