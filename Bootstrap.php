c138a7bfb6dc01ca0d96467ec1a5cd3e35e97764
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2020-08-05 15:34:35 +0200

    Updated signature

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,16 +1,18 @@
 <?php
 namespace RpcViewer;
 
+use DI\Container;
 use Jtl\Connector\Core\Config\ConfigSchema;
 use Jtl\Connector\Core\Config\GlobalConfig;
 use Jtl\Connector\Core\Definition\Event;
 use Jtl\Connector\Core\Plugin\PluginInterface;
+use Noodlehaus\ConfigInterface;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use RpcViewer\Listener\RpcListener;
 
 class Bootstrap implements PluginInterface
 {
-    public function registerListener(EventDispatcher $dispatcher)
+    public function registerListener(ConfigInterface $config, Container $container, EventDispatcher $dispatcher)
     {
         $logDir = sprintf('%s/var/log', dirname(dirname(__DIR__)));
         $configFile = sprintf('%s/config/config.php', __DIR__);
